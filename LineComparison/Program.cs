a687fc3c6b357813bb8aa170c752ea535f54d4a4
Vivek Verma <vverma240420@gmail.com>
2023-02-23 00:59:01 +0530

    Length Of Line

---
+++ LineComparison/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LineComparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value for cooridinate x1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate y1");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate x2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value for cooridinate y2");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            double LengthOfLine1 = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Welcome to the Line Comparison Problems");
+            Console.WriteLine("Length of Line:" + LengthOfLine1);
+
+            Console.ReadLine();
+        }
+    }
+}
